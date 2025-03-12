952adabfc13d013afe96ec59d319c7c7103e8aca
denismikhno <denismikhno@gmail.com>
2025-03-12 16:03:34 +0300

    Выполнил homework.interfaces.task_1

--- src/java/homework/interfaces/task_1/Converter.java
+++ src/java/homework/interfaces/task_1/Converter.java
@@ -7,6 +7,9 @@ public interface Converter {
         // TODO
         // в зависимости от degree (CELSIUS,FAHRENHEIT, KELVIN)
         // верни конкретный экземпляр нужного класса
+        if (Degree.CELSIUS == degree) return new CelsiusConverter();
+        if (Degree.KELVIN == degree) return new KelvinConverter();
+        if (Degree.FAHRENHEIT == degree) return new FahrenheitConverter();
         return null;
     }
 }
