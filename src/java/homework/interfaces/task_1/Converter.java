307766a592d71288701d9ec71f6921d6a9f8caaf
denismikhno <denismikhno@gmail.com>
2025-03-12 13:20:00 +0300

    Добавил homework.interfaces.task_1

---
+++ src/java/homework/interfaces/task_1/Converter.java
@@ -0,0 +1,13 @@
+package homework.interfaces.task_1;
+
+public interface Converter {
+    double convert(double baseValue);
+
+    public static Converter getInstance(Degree degree) {
+        // TODO
+        // в зависимости от degree (CELSIUS,FAHRENHEIT, KELVIN)
+        // верни конкретный экземпляр нужного класса
+        return null;
+    }
+}
+
