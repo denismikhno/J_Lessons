bf7595e1e0e15eb38f282ec321d1c1271000ed0a
denismikhno_laptop <denismikhno@gmail.com>
2025-03-18 20:16:37 +0300

    Выполнил homework.classes.statics.task_1 полностью

---
+++ src/java/homework/statics/task_1/Rose.java
@@ -0,0 +1,8 @@
+package homework.statics.task_1;
+
+public class Rose extends Flower {
+
+    public Rose(String name, String countryOfOrigin, int shelfLifeInDays, int pricePerPiece) {
+        super(name, countryOfOrigin, shelfLifeInDays, pricePerPiece);
+    }
+}
---
+++ src/java/homework/statics/task_1/Tulip.java
@@ -0,0 +1,8 @@
+package homework.statics.task_1;
+
+public class Tulip extends Flower {
+
+    public Tulip(String name, String countryOfOrigin, int shelfLifeInDays, int pricePerPiece) {
+        super(name, countryOfOrigin, shelfLifeInDays, pricePerPiece);
+    }
+}
